d26a33a67bbaa23a6b92bf868f43db8481558138
p_ilxiangli <p_ilxiangli@tencent.com>
2024-04-06 16:31:16 +0800

    路由demo提交

---
+++ RouterDemo/LoadModel.h
@@ -0,0 +1,17 @@
+//
+//  LoadModel.h
+//  RouterDemo
+//
+//  Created by 李祥 on 2024/4/6.
+//  Copyright © 2024 廖文韬. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoadModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RouterDemo/LoadModel.m
@@ -0,0 +1,18 @@
+//
+//  LoadModel.m
+//  RouterDemo
+//
+//  Created by 李祥 on 2024/4/6.
+//  Copyright © 2024 廖文韬. All rights reserved.
+//
+
+#import "LoadModel.h"
+
+@implementation LoadModel
+
++ (void)load {
+    
+    
+    NSLog(@"测试load加载机制");
+}
+@end
